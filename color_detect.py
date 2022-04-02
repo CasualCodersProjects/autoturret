725b5c08ec219c3089bc6bb42956727b8eeb06e7
chand1012 <chandler@chand1012.dev>
2022-04-02 13:11:19 -0400

    Add pixel averaging

--- color_detect.py
+++ color_detect.py
@@ -5,6 +5,12 @@ import numpy as np
 
 def main():
 
+    rect_width = 50
+    rect_height = 50
+
+    frame_counter = 0
+    frame_counter_max = 10
+
     video_capture = cv2.VideoCapture(0)
     video_width = int(video_capture.get(3))
     video_height = int(video_capture.get(4))
@@ -19,16 +25,34 @@ def main():
 
         reds = []
 
-        reds = np.argwhere(cv2.inRange(frame, (0, 0, 175), (50, 50, 255)))
+        reds = np.argwhere(cv2.inRange(frame, (0, 0, 125), (50, 50, 255)))
+
+        # for px, py in reds:
+        #     red_value = frame[px, py, 0]
+        #     red = (red_value, py, px)
+        #     if red[0] > most_red[0]:
+        #         most_red = red
+
+        xs = []
+        ys = []
 
         for px, py in reds:
-            red_value = frame[px, py, 0]
-            red = (red_value, py, px)
-            if red[0] > most_red[0]:
-                most_red = red
+            xs.append(px)
+            ys.append(py)
+
+        if len(xs) > 0 and len(ys) > 0:
+            avg_x = np.mean(xs)
+            avg_y = np.mean(ys)
+
+            if avg_x > 0 and avg_y > 0:
+                most_red = (0, int(avg_y), int(avg_x))
 
         if most_red != (0, 0, 0):
-            cv2.circle(frame, (most_red[1], most_red[2]), 10, (0, 0, 255), -1)
+            # cv2.circle(frame, (most_red[1], most_red[2]), 10, (0, 0, 255), -1)
+            cv2.rectangle(frame, (most_red[1] - rect_width // 2, most_red[2] - rect_height // 2),
+                          (most_red[1] + rect_width // 2, most_red[2] + rect_height // 2), (0, 0, 255), 2)
+            cv2.putText(frame, "Hostile Detected",
+                        (most_red[1] - 5 - rect_width // 2, most_red[2] - 15 - rect_height // 2), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
 
         cv2.imshow('Video', frame)
 

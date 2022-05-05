526988da232d04f7f961f5b56160d03fb7ffb842
chand1012 <chandler@chand1012.dev>
2022-05-05 11:49:28 -0400

    Add cleanup

--- control.py
+++ control.py
@@ -1,11 +1,15 @@
 # tests controlling the steppers
 # import keyboard
 # from stepper_control import Stepper
+import RPi.GPIO as GPIO
 
-x_stepper = Stepper(step_pin=12, direction_pin=16, enable_pin=8)
-# y_stepper = Stepper(step_pin=38, direction_pin=40, enable_pin=8)
+try:
+    x_stepper = Stepper(step_pin=12, direction_pin=16, enable_pin=8)
+    # y_stepper = Stepper(step_pin=38, direction_pin=40, enable_pin=8)
 
-x_stepper.setup()
+    x_stepper.setup()
 
-while True:
-    x_stepper.step(direction=1, steps=200)
+    while True:
+        x_stepper.step(direction=1, steps=200)
+except KeyboardInterrupt:
+    GPIO.cleanup()

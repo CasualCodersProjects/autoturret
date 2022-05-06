a1e381b691d79447aa2912e703000644ee131d0f
chand1012 <chandler@chand1012.dev>
2022-05-06 17:53:26 -0400

    Add improved detection algorithm

---
+++ detect.py
@@ -0,0 +1,56 @@
+import cv2
+import numpy as np
+
+LOWER_MASK_LOWER_RED = np.array([0, 50, 20])
+LOWER_MASK_UPPER_RED = np.array([5, 255, 255])
+UPPER_MASK_LOWER_RED = np.array([175, 50, 20])
+UPPER_MASK_UPPER_RED = np.array([180, 255, 255])
+
+# great resource: http://vikrantfernandes.com/web/html/Color-track.html
+
+
+def detect_hostiles(frame, lower_mask_lower_red=LOWER_MASK_LOWER_RED, lower_mask_upper_red=LOWER_MASK_UPPER_RED, upper_mask_lower_red=UPPER_MASK_LOWER_RED, upper_mask_upper_red=UPPER_MASK_UPPER_RED):
+    blurred_frame = cv2.GaussianBlur(frame, (5, 5), 0)
+    # convert to hsv
+    hsv = cv2.cvtColor(blurred_frame, cv2.COLOR_BGR2HSV)
+
+    # create kernel
+    kernel = np.ones((5, 5), np.uint8)
+
+    # create mask
+    mask = cv2.inRange(hsv, lower_mask_lower_red, lower_mask_upper_red)
+    mask += cv2.inRange(hsv, upper_mask_lower_red, upper_mask_upper_red)
+
+    # morphological opening
+    mask = cv2.erode(mask, kernel, iterations=2)
+    mask = cv2.dilate(mask, kernel, iterations=2)
+    # morphological closing
+    mask = cv2.dilate(mask, kernel, iterations=2)
+    mask = cv2.erode(mask, kernel, iterations=2)
+
+    cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,
+                            cv2.CHAIN_APPROX_SIMPLE)[-2]
+
+    if len(cnts) <= 0:
+        return (-1, -1, 0, mask)
+
+    # Contour with greatest area
+    c = max(cnts, key=cv2.contourArea)
+    # Radius and center pixel coordinate of the largest contour
+    ((x, y), radius) = cv2.minEnclosingCircle(c)
+
+    return (int(x), int(y), radius, mask)
+
+
+if __name__ == "__main__":
+    video_capture = cv2.VideoCapture(0)
+
+    while True:
+        _, frame = video_capture.read()
+        x, y, radius, mask = detect_hostiles(frame)
+        cv2.circle(frame, (int(x), int(y)), int(radius), (0, 255, 0), 2)
+        print(x, y, radius)
+        cv2.imshow("Video", frame)
+        # cv2.imshow("Mask", mask)
+        if cv2.waitKey(1) & 0xFF == ord("q"):
+            break
--- main.py
+++ main.py
@@ -1,90 +1,65 @@
-import numpy as np
-import cv2
-import time
 import platform
 
-LOWER_MASK_LOWER_RED = np.array([0, 50, 20])
-LOWER_MASK_UPPER_RED = np.array([5, 255, 255])
-UPPER_MASK_LOWER_RED = np.array([175, 50, 20])
-UPPER_MASK_UPPER_RED = np.array([180, 255, 255])
-RECT_WIDTH = 50
-RECT_HEIGHT = 50
+import cv2
+
+from detect import detect_hostiles
+
+THRESHOLD = 50
 
 
-def to_hsv(frame):
-    return cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-
-
-def convert_bgr_to_hsv(b, g, r):
-    h, s, v = cv2.cvtColor(np.array([[[b, g, r]]]), cv2.COLOR_BGR2HSV)[0][0]
-    return h, s, v
-
-
-def get_reds(frame):
-    mask = cv2.inRange(frame, LOWER_MASK_LOWER_RED, LOWER_MASK_UPPER_RED)
-    mask = mask + cv2.inRange(frame, UPPER_MASK_LOWER_RED,
-                              UPPER_MASK_UPPER_RED)
-    reds = np.argwhere(mask)
-    return reds
-
-
-def get_pixel_average(arr):
-    xs = []
-    ys = []
-    for px, py in arr:
-        xs.append(px)
-        ys.append(py)
-    if len(xs) > 0 and len(ys) > 0:
-        avg_x = np.mean(xs)
-        avg_y = np.mean(ys)
-        return (int(avg_y), int(avg_x))
-    return (0, 0)
-
-
-def get_pixel_max(arr, frame):
-    max_hue_value = np.array([-1, -1, -1])
-    max_frame_location = np.array([-1, -1])
-    for py, px in arr:
-        frame_hue_value = frame[py, px]
-        hue = frame_hue_value[0] if frame_hue_value[0] < max_hue_value[0] else max_hue_value[0]
-        saturation = frame_hue_value[1] if frame_hue_value[1] > max_hue_value[1] else max_hue_value[1]
-        value = frame_hue_value[2] if frame_hue_value[2] > max_hue_value[2] else max_hue_value[2]
-        new_hue_value = np.array([hue, saturation, value])
-        if np.array_equal(new_hue_value, max_hue_value):
-            max_hue_value = new_hue_value
-            max_frame_location = np.array([px, py])
-    return max_frame_location
-
-
-def draw_hostile_box(frame, target):
-    cv2.rectangle(frame, (target[0] - RECT_WIDTH // 2, target[1] - RECT_HEIGHT // 2),
-                  (target[0] + RECT_WIDTH // 2, target[1] + RECT_HEIGHT // 2), (0, 0, 255), 2)
+def draw_hostile_box(frame, target, radius):
+    cv2.rectangle(frame, (target[0] - radius // 2, target[1] - radius // 2),
+                  (target[0] + radius // 2, target[1] + radius // 2), (0, 0, 255), 2)
     cv2.putText(frame, "Hostile Detected",
-                (target[0] - 5 - RECT_WIDTH // 2, target[1] - 15 - RECT_HEIGHT // 2), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
+                (target[0] - 5 - radius // 2, target[1] - 15 - radius // 2), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
 
 
 if __name__ == "__main__":
     video_capture = cv2.VideoCapture(0)
 
-    lastPrint = 0
+    _, frame = video_capture.read()
+
+    # get the frame height and width
+    height, width = frame.shape[:2]
+
+    # get the center
+    center_x = width // 2
+    center_y = height // 2
+
+    threshold_y = 50
+    threshold_x = 50
+
+    shoot_range_x = range(center_x - threshold_x, center_x + threshold_x + 1)
+    shoot_range_y = range(center_y - threshold_y, center_y + threshold_y + 1)
+
+    print("Video width: ", width)
+    print("Video height: ", height)
+    print("Center x: ", center_x)
+    print("Center y: ", center_y)
+    print("Threshold x: ", threshold_x)
+    print("Threshold y: ", threshold_y)
+    print("Shoot range x: ", shoot_range_x)
+    print("Shoot range y: ", shoot_range_y)
 
     while True:
         _, frame = video_capture.read()
 
-        hsv_frame = to_hsv(frame)
+        x, y, r, mask = detect_hostiles(frame)
 
-        reds = get_reds(hsv_frame)
-        # target = get_pixel_average(reds)
-        target = get_pixel_max(reds, hsv_frame)
+        print(x, y, r)
 
-        if target.all() != -1:
-            draw_hostile_box(frame, target)
-            # print the target every second
-            if lastPrint < time.time() - 1:
-                print(target)
-                b, g, r = frame[target[1], target[0]]
-                print(convert_bgr_to_hsv(b, g, r))
-                lastPrint = time.time()
+        if r > THRESHOLD:
+            draw_hostile_box(frame, (int(x), int(y)), int(r))
+            if x > center_x:
+                print("Hostile is to the right")
+            elif x < center_x:
+                print("Hostile is to the left")
+            if y > center_y:
+                print("Hostile is above")
+            elif y < center_y:
+                print("Hostile is below")
+            if x in shoot_range_x and y in shoot_range_y:
+                print("Shooting")
 
         if not 'arm' in platform.machine():
             cv2.imshow('Video', frame)

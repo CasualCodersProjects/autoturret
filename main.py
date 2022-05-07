4d314594113dbdeeb1d75565991ea95e7d1a5eb9
chand1012 <chandler@chand1012.dev>
2022-05-06 20:29:13 -0400

    Add command line parameters

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import platform
 
 import cv2
+import fire
 
 from detect import detect_hostiles
 
@@ -14,7 +14,7 @@ def draw_hostile_box(frame, target, radius):
                 (target[0] - 5 - radius // 2, target[1] - 15 - radius // 2), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
 
 
-if __name__ == "__main__":
+def sentry(dry_run=False, verbose=False, display_mask=False, scale=0, output_scale=0):
     video_capture = cv2.VideoCapture(0)
 
     _, frame = video_capture.read()
@@ -44,25 +44,51 @@ if __name__ == "__main__":
     while True:
         _, frame = video_capture.read()
 
+        if scale:
+            # scale the image
+            frame = cv2.resize(
+                frame, (int(width * scale), int(height * scale)))
+
         x, y, r, mask = detect_hostiles(frame)
 
-        print(x, y, r)
+        if verbose:
+            print(x, y, r)
 
         if r > THRESHOLD:
             draw_hostile_box(frame, (int(x), int(y)), int(r))
             if x > center_x:
-                print("Hostile is to the right")
+                if dry_run:
+                    if verbose:
+                        print("Hostile is to the right")
             elif x < center_x:
-                print("Hostile is to the left")
+                if dry_run:
+                    if verbose:
+                        print("Hostile is to the left")
             if y > center_y:
-                print("Hostile is above")
+                if dry_run:
+                    if verbose:
+                        print("Hostile is below")
             elif y < center_y:
-                print("Hostile is below")
+                if dry_run:
+                    if verbose:
+                        print("Hostile is above")
             if x in shoot_range_x and y in shoot_range_y:
-                print("Shooting")
+                if dry_run:
+                    if verbose:
+                        print("Shooting")
 
-        if not 'arm' in platform.machine():
-            cv2.imshow('Video', frame)
+        if dry_run:
+            if display_mask:
+                cv2.imshow("Mask", mask)
+            else:
+                if output_scale:
+                    frame = cv2.resize(
+                        frame, (int(width * output_scale), int(height * output_scale)))
+                cv2.imshow('Video', frame)
 
             if cv2.waitKey(1) & 0xFF == ord('q'):
                 break
+
+
+if __name__ == "__main__":
+    fire.Fire(sentry)
